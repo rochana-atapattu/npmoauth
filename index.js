d0499272151afea58204b61c51be71b2388de5cb
rochana <rochana.atapattu2@gmail.com>
2019-09-26 22:23:06 +0530

    docker

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const Express=require('express');
 const BodyParser=require('body-parser');
 const Routes=require('./Routes');
 const app=Express();
+var path = require('path');
 
 app.use(BodyParser.json());
 //app.use('/Routes',Routes);
 app.get('/', function(request, response){
-    response.sendFile('/home/rochana/Documents/SSD/NodeOAuth/index.html');
+    response.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 
-app.listen(8000,'localhost',function (err) {
+app.listen(8000,'0.0.0.0',function (err) {
     if(err){
         console.log(err);
         process.exit(-1);
